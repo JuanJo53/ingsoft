98761f0e1656a24f3dd0e4aaa6390314f77c71ab
Juan Jose Fernandez <juanjofer1999@gmail.com>
2020-10-29 17:15:36 -0400

    1 Insert for each table added

--- database.sql
+++ database.sql
@@ -393,3 +393,48 @@ ALTER TABLE user_tags ADD CONSTRAINT user_tags_user FOREIGN KEY user_tags_user (
 
 -- End of file.
 
+-- Inserts for area table
+INSERT INTO area VALUES (null, "Startup de tecnologia",now());
+
+-- Inserts for bill table
+INSERT INTO bill VALUES (null,1,1,1,1,1,now(),"c/Azul Nro. 123","Bolivia","Tarija",1,"192.168.1.1",1, now());
+
+-- Inserts for card table
+INSERT INTO card VALUES (null,1,"Tarjeta personal", 13456898453, 2020, 02, "Juan", "Perez",359,now(),1,"192.168.1.1",1, now());
+
+-- Inserts for certificate table
+INSERT INTO certificate VALUES (null, 1, "Curso Python Basico", "Udemy","10-02-2015","c9dd7d32e6548e9a3aa3669","https://udemy.com/@albert/certificados/curso-python",now(),1,"192.168.1.1",1, now());
+
+-- Inserts for media table
+INSERT INTO media VALUES (null,1,1,"http://photos.com/super.png", 2,now());
+
+-- Inserts for notification table
+INSERT INTO notification VALUES (null,1,now(),"Nuevo interesado","Un usario esta interesado en tu proyecto",5);
+
+-- Inserts for payment_plan table
+INSERT INTO payment_plan VALUES (null,"Plan 1","Plan por publicidad por un mes para tu proyecto",100.0,5,now(),1,"192.168.1.1",1, now());
+
+-- Inserts for project_user table
+INSERT INTO project_user VALUES (null,1,1,0,0,1,"192.168.1.1",1, now());
+
+-- Inserts for projects table
+INSERT INTO projects VALUES (null,"Media Mart App","Aplicacion para obtener multimedia mas facilmente","Se requiere programador backend y programador frontend experimentados en tecnologias web","2% en acciones a los que se unan a nuestro equipo",0,0,now(),1,"192.168.1.1",1, now());
+
+-- Inserts for projects_area table
+INSERT INTO project_area VALUES (null,1,1);
+
+-- Inserts for projects_tags table
+INSERT INTO project_tags VALUES (null,1,1);
+
+-- Inserts for tags table
+INSERT INTO tags VALUES (null, "python",1);
+
+-- Inserts for user table
+INSERT INTO user VALUES (null, "Albert", "Molina", "@almola","moli147@gmail.com","c9dd7d32e8e9a3aa3669","","","",1,"192.168.1.1",1, now());
+
+-- Inserts for user_tags table
+INSERT INTO user_tags VALUES (null,1,1);
+
+
+
+
