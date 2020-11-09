0420df76802a1a25cd1dc2b2524ec125ebea419b
coriccf <corina.colque.f@gmail.com>
2020-11-09 15:58:14 -0400

    adding CardRequest

---
+++ src/main/java/bo/ucb/edu/ingsoft/dto/CardRequest.java
@@ -0,0 +1,85 @@
+package bo.ucb.edu.ingsoft.dto;
+
+import bo.ucb.edu.ingsoft.model.Transaction;
+
+import java.util.Date;
+
+public class CardRequest {
+
+
+    private String cardName;
+    private Integer cardNumber;
+    private Integer expirationYear;
+    private Integer expirationMonth;
+    private String userName;
+    private String userSurname;
+    private Integer cvc;
+    private Date creationDate;
+
+    public CardRequest() {
+    }
+
+    public String getCardName() {
+        return cardName;
+    }
+
+    public void setCardName(String cardName) {
+        this.cardName = cardName;
+    }
+
+    public Integer getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(Integer cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public Integer getExpirationYear() {
+        return expirationYear;
+    }
+
+    public void setExpirationYear(Integer expirationYear) {
+        this.expirationYear = expirationYear;
+    }
+
+    public Integer getExpirationMonth() {
+        return expirationMonth;
+    }
+
+    public void setExpirationMonth(Integer expirationMonth) {
+        this.expirationMonth = expirationMonth;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserSurname() {
+        return userSurname;
+    }
+
+    public void setUserSurname(String userSurname) {
+        this.userSurname = userSurname;
+    }
+
+    public Integer getCvc() {
+        return cvc;
+    }
+
+    public void setCvc(Integer cvc) {
+        this.cvc = cvc;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+}
