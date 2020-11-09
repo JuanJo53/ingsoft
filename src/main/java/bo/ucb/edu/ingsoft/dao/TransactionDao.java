5539c552e5e9b646835be8b08080c60575a7016a
Juan Jose Fernandez <juanjofer1999@gmail.com>
2020-11-08 20:15:56 -0400

    result map for certificates

--- src/main/java/bo/ucb/edu/ingsoft/dao/TransactionDao.java
+++ src/main/java/bo/ucb/edu/ingsoft/dao/TransactionDao.java
@@ -2,6 +2,7 @@ package bo.ucb.edu.ingsoft.dao;
 
 import bo.ucb.edu.ingsoft.model.Transaction;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.ResultMap;
 
 @Mapper
 public interface TransactionDao {
