bf136cd0d16cdb2339e2e225539cca869970bcc6
Juan Jose Fernandez <juanjofer1999@gmail.com>
2020-11-08 22:43:26 -0400

    PUT for certificates done, update for an specific certificate

--- src/main/java/bo/ucb/edu/ingsoft/api/CertificateApi.java
+++ src/main/java/bo/ucb/edu/ingsoft/api/CertificateApi.java
@@ -3,15 +3,13 @@ package bo.ucb.edu.ingsoft.api;
 import bo.ucb.edu.ingsoft.bl.CertificateBl;
 import bo.ucb.edu.ingsoft.bl.TransactionBl;
 import bo.ucb.edu.ingsoft.dto.CertificateRequest;
+import bo.ucb.edu.ingsoft.dto.ProjectRequest;
 import bo.ucb.edu.ingsoft.model.Certificate;
 import bo.ucb.edu.ingsoft.model.Transaction;
 import bo.ucb.edu.ingsoft.util.TransactionUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
@@ -41,6 +39,18 @@ public class CertificateApi {
         Transaction transaction = TransactionUtil.createTransaction(request);
         transactionBl.createTransaction(transaction);
         CertificateRequest certificateResponse = certificateBl.createCertificate(1,certificateRequest, transaction);
+        return certificateResponse;
+    }
+
+    @RequestMapping(value = "/edit/{certificateid}" ,method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public CertificateRequest editCertificate(@PathVariable("certificateid") Integer certificateId, @RequestBody CertificateRequest certificateRequest, HttpServletRequest request) {
+        // Creamos transaccion para la operación.
+        Transaction transaction = TransactionUtil.createTransaction(request);
+        transactionBl.createTransaction(transaction);
+
+        CertificateRequest certificateResponse=certificateBl.editCertificate(certificateRequest,certificateId,transaction);
+        //CertificateRequest certificateResponse = certificateBl.createCertificate(1,certificateRequest, transaction);
         return certificateResponse;
     }
 }
--- src/main/java/bo/ucb/edu/ingsoft/bl/CertificateBl.java
+++ src/main/java/bo/ucb/edu/ingsoft/bl/CertificateBl.java
@@ -1,12 +1,10 @@
 package bo.ucb.edu.ingsoft.bl;
 
-import bo.ucb.edu.ingsoft.api.CertificateApi;
 import bo.ucb.edu.ingsoft.dao.CertificateDao;
 import bo.ucb.edu.ingsoft.dao.TransactionDao;
 import bo.ucb.edu.ingsoft.dto.CertificateRequest;
 import bo.ucb.edu.ingsoft.model.Certificate;
 import bo.ucb.edu.ingsoft.model.Transaction;
-import bo.ucb.edu.ingsoft.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,9 +26,11 @@ public class CertificateBl {
     public Certificate detailsByCertificateId(Integer certificateId) {
         return  certificateDao.detailsByCertificateId(certificateId);
     }
+    //Function that gets all user certificates
     public List<Certificate> getCertificateBasicData(Integer userId) {
         return  certificateDao.getCertificates(userId);
     }
+
     public CertificateRequest createCertificate(Integer userId, CertificateRequest certificateRequest, Transaction transaction) {
         Certificate certificate = new Certificate();
 
@@ -48,6 +48,32 @@ public class CertificateBl {
         certificate.setTransaction(transaction);
 
         certificateDao.newCertificate(certificate);
+
+        return  certificateRequest;
+    }
+
+    public CertificateRequest editCertificate(CertificateRequest certificateRequest, Integer certificateId, Transaction transaction) {
+        Certificate certificate = new Certificate();
+
+        certificate.setCertificateId(certificateId);
+        certificate.setName(certificateRequest.getName());
+        certificate.setCompany(certificateRequest.getCompany());
+        certificate.setExpeditionDate(certificateRequest.getExpeditionDate());
+        certificate.setCredentialId(certificateRequest.getCredentialId());
+        certificate.setCredentialURL(certificateRequest.getCredentialURL());
+        certificate.setExpirationDate(certificateRequest.getExpirationDate());
+
+        LOGGER.error(certificate.getCertificateId().toString());
+        LOGGER.error(certificate.getName());
+        LOGGER.error(certificate.getCompany());
+        LOGGER.error(certificate.getExpirationDate().toString());
+        LOGGER.error(certificate.getCredentialId());
+        LOGGER.error(certificate.getCredentialURL());
+        LOGGER.error(certificate.getExpeditionDate().toString());
+
+        certificate.setTransaction(transaction);
+
+        certificateDao.updateCertificate(certificate);
 
         return  certificateRequest;
     }
