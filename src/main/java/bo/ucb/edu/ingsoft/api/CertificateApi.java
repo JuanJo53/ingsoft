259f4b697adf899028c12e9e41e3136916f88136
Juan Jose Fernandez <juanjofer1999@gmail.com>
2020-11-12 20:11:29 -0400

    certificate small fix

--- src/main/java/bo/ucb/edu/ingsoft/api/CertificateApi.java
+++ src/main/java/bo/ucb/edu/ingsoft/api/CertificateApi.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/my")
+@RequestMapping(value = "/user")
 public class CertificateApi {
 
     private CertificateBl certificateBl;
